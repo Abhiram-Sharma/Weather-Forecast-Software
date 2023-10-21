e1de80447bd717f56b054319836dcb4046aba06b
Mohammad-Afzal123 <143783000+Mohammad-Afzal123@users.noreply.github.com>
2023-10-21 19:40:49 +0530

    Create Get_Weatherfunction.py

---
+++ Get_Weatherfunction.py
@@ -0,0 +1,20 @@
+def Get_Weather(city,date):
+  h=''        #Humidity
+  t=''        #Temperature
+  fl=''       #FeelsLike
+  ws=''        #Windspeed
+  cl=''        #Cloud
+  pr=''         #Pressure
+  # write code here
+
+
+  
+  result={
+    "Hum":h1,
+    "Temp":t1,
+    "Feels":fl1,
+    "Wind":ws1,
+    "Cloud":cl1,
+    "Pressure":pr1,
+  }
+  return result
