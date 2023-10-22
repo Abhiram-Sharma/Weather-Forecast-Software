b883b230b13e80718ccea2342667623bc3dd0b8f
Abhiram Sharma <142007229+Abhiram-Sharma@users.noreply.github.com>
2023-10-22 21:41:52 +0530

    Update weather forecast.py

--- weather forecast.py
+++ weather forecast.py
@@ -1,11 +1,12 @@
-theme_menu=['SandyBeach','Light brown 13','python','black','random']
+# theme_menu=['SandyBeach','Light brown 13','python','black','random']
 
 import PySimpleGUI as sg
+from Get_Weatherfunction import Get_Weather as GW
 
 # Define the layout
 layout = [
-    [sg.Text('City:'), sg.Input(key='city')],
-    [sg.Text('Country:'), sg.Input(key='country')],
+    [sg.Text('City :'), sg.InputText(key='city')],
+    [sg.Text('Date :'), sg.InputText(key='Date'),sg.CalendarButton("Select Date",close_when_date_chosen=True,target="Date",format='%Y-%m-%d')],
     [sg.Button('Get Weather'), sg.Button('Exit')],
     [sg.Output(size=(80, 20))]
 ]
@@ -24,11 +25,29 @@ while True:
 
     if event == 'Get Weather':
         city = values['city']
-        country = values['country']
+        DateT = values['Date']
 
         # fetch the weather data
+        '''
+        result={
+            "Humidity":h1,
+            "Temp":t1,
+            "FeelsLike":fl1,
+            "Wind":ws1,
+            "Cloud":cl1,
+            "Pressure":pr1,
+            "Rain":rn1
+            }
+        '''
+        wdata=GW(city,DateT)
+        print(f"Weather Data for {city} on {DateT} :")
+        print(f"Humidity : {wdata['Humidity']}")
+        print(f"Windspeed : {wdata['Wind']}")
+        print(f"Clouds : {wdata['Cloud']} %")
+        print(f"Pressure : {wdata['Pressure']}")
+        print(f"Rain : {wdata['Rain']}")
+        print(f"Temperature : {wdata['Temp']}")
+        print(f"Feels Like : {wdata['FeelsLike']}")
 
-        weather_data = "Weather data for " + city + ", " + country
-        print(weather_data)
 
 window.close()
