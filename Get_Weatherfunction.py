ddff15b13ffb800895821d05bc330265b026140f
Mohammad-Afzal123 <143783000+Mohammad-Afzal123@users.noreply.github.com>
2023-10-22 23:07:16 +0530

    Update Get_Weatherfunction.py

--- Get_Weatherfunction.py
+++ Get_Weatherfunction.py
@@ -82,23 +82,13 @@ def Get_Weather(city,dateT):
     # Getting date Range
     import datetime
     from datetime import date as dt                            #Import date from datetime library
-    def loop(n):
-        if n<=1:
-            if n<=0:                                        
-                print('The weather on '+date2+' is:')      #In case the date is before current day
-            return 1                                     
-        else:
-            base=dt.today()                              #alloting base date
-            print(base+datetime.timedelta(days=n))       #printing the days
-            return n*loop(n-1)                              
-    date1=str(dt.today())                                #Taking Today's Date as Date1
-    date2=input('Enter the second date: ')                 #Taking date entered as Date2
-    datetime1=dt.fromisoformat(date1)                    #Taking date1 in numerical form
-    datetime2=dt.fromisoformat(date2)                    #Taking date2 in numerical form
-    x=str(datetime2-datetime1)                             #Substracting two dates
-    l=x.split()                                            #spliting x into ['No of days','days','00:00:00']                                 
-    print(loop(int(l[0])))                                 #considering No of days in the list and calling the function from the loop function
-  
+    def NoDays(date2):                   
+      date1=str(dt.today()-datetime.timedelta(days=1))                                #Taking Today's Date as Date1
+      datetime1=dt.fromisoformat(date1)                    #Taking date1 in numerical form
+      datetime2=dt.fromisoformat(date2)                    #Taking date2 in numerical form
+      x=str(datetime2-datetime1)                             #Substracting two dates
+      l=x.split()                                            #spliting x into ['No of days','days','00:00:00']                                 
+      return int(l[0]) if int(l[0])>0 else 0
     #Weighted average calculation
     def weightedAvg(d1,d2,d3,d4,d5):
       d=((d1*9)+(d2*6)+(d3*5)+(d4*4)+(d5*2))/26
@@ -126,7 +116,7 @@ def Get_Weather(city,dateT):
           return weightedAvg(param_n_minus_1, param_n_minus_2, param_n_minus_3, param_n_minus_4 ,param_n_minus_5)
     
     #define n here, number of days between today and the target day +5
-    n=""+5
+    n=NoDays(dateT)+5
 
     h1=paramCalc(n,d1.h,d2.h,d3.h,d4.h,d5.h)
     t1=paramCalc(n,d1.t,d2.t,d3.t,d4.t,d5.t)
