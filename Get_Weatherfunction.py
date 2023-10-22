a4884200710203b4d767911ea46c667a95d748f9
Mohammad-Afzal123 <143783000+Mohammad-Afzal123@users.noreply.github.com>
2023-10-22 09:46:33 +0530

    Update Get_Weatherfunction.py

--- Get_Weatherfunction.py
+++ Get_Weatherfunction.py
@@ -14,13 +14,13 @@ def loop(n):
     else:
         
         return n*loop(n-1)
-date1=str(date.today())
-date2=input('Enter the second date: ')
-datetime1=date.fromisoformat(date1)
-datetime2=date.fromisoformat(date2)
-x=str(datetime2-datetime1)
-l=x.split()   
-print(loop(int(l[0]))) 
+date1=str(date.today())                                #Taking Today's Date as Date1
+date2=input('Enter the second date: ')                 #Taking date entered as Date2
+datetime1=date.fromisoformat(date1)                    #Taking date1 in numerical form
+datetime2=date.fromisoformat(date2)                    #Taking date2 in numerical form
+x=str(datetime2-datetime1)                             #Substracting two dates
+l=x.split()                                            #spliting x into ['No of days','days','00:00:00']
+print(loop(int(l[0])))                                 #considering No of days in the list and calling the function from the loop function
   result={
     "Hum":h1,
     "Temp":t1,
